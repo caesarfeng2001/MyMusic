967f620ff29a7657487278cfe518a25b1558cda9
Caesar Feng <c29feng@uwaterloo.ca>
2021-03-21 00:02:12 -0400

    artist model

--- MyMusic.Core/Models/Artist.cs
+++ MyMusic.Core/Models/Artist.cs
@@ -1,7 +1,17 @@
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
 namespace MyMusic.Core.Models
 {
-    public class ArtistId
+    public class Artist
     {
-        
+        public Artist()
+        {
+            Musics = new Collection<Music>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Music> Musics { get; set; }
     }
 }
